d530d5977abab74d2948145e41a8787908b6abe2
Juandvr <villabonajuan203@gmail.com>
2026-07-02 23:16:57 -0500

    save todos to localStorage using the web storage API

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -1,11 +1,17 @@
 import './classes.js'
 import { ToDo } from './classes.js'
 
-const todos = []
+let todos = []
 
 function createTodo(data) {
     const newTodo = new ToDo('default', data.title, data.description, data.dueDate, data.priority)
+
+    if (localStorage.getItem('todos')) {
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+    
     todos.push(newTodo)
+    localStorage.setItem('todos', JSON.stringify(todos))
     console.log(todos)
 }
 
