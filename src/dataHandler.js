8a6752f74f3c897309f70211df2291a1dd67032e
Juandvr <villabonajuan203@gmail.com>
2026-07-04 19:21:42 -0500

    add function to load todos from the localStorage

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -1,18 +1,22 @@
 import './classes.js'
 import { ToDo } from './classes.js'
 
-let todos = []
+let todos = JSON.parse(localStorage.getItem('todos')) || []
 
 function createTodo(data) {
-    const newTodo = new ToDo('default', data.title, data.description, data.dueDate, data.priority)
-
-    if (localStorage.getItem('todos')) {
-        todos = JSON.parse(localStorage.getItem('todos'))
-    }
-
+    const newTodo = new ToDo(data.title, data.description, data.dueDate, data.priority)
     todos.push(newTodo)
     localStorage.setItem('todos', JSON.stringify(todos))
     console.log(todos)
 }
 
-export { createTodo, todos }
+function loadTodos() {
+    if (todos != []) {
+        todos = todos.map(data => {
+            const newTodo = new ToDo()
+            return Object.assign(newTodo, data)
+        }) 
+    }
+}
+
+export { createTodo, loadTodos, projects, todos }
